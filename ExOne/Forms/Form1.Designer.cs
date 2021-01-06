2a4d0939c826c5e4740a8efaae56aeb41172e756
Restu Adi Pradana <restu.maduki@live.com>
2021-01-06 12:04:31 +0700

    3rd change

--- ExOne/Forms/Form1.Designer.cs
+++ ExOne/Forms/Form1.Designer.cs
@@ -29,38 +29,31 @@ namespace ExOne
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
+            this.UploadBtn = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BackBtn = new System.Windows.Forms.Button();
+            this.L2 = new System.Windows.Forms.Button();
+            this.L3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(713, 386);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Insert DB";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(713, 415);
+            this.button2.Location = new System.Drawing.Point(727, 607);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Refresh";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(37, 391);
+            this.button3.Location = new System.Drawing.Point(25, 604);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -70,40 +63,81 @@ namespace ExOne
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(118, 394);
+            this.textBox1.Location = new System.Drawing.Point(106, 607);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(394, 20);
             this.textBox1.TabIndex = 3;
             // 
-            // button4
+            // UploadBtn
             // 
-            this.button4.Location = new System.Drawing.Point(518, 391);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Upload";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.UploadBtn.Location = new System.Drawing.Point(506, 604);
+            this.UploadBtn.Name = "UploadBtn";
+            this.UploadBtn.Size = new System.Drawing.Size(75, 23);
+            this.UploadBtn.TabIndex = 4;
+            this.UploadBtn.Text = "Upload";
+            this.UploadBtn.UseVisualStyleBackColor = true;
+            this.UploadBtn.Click += new System.EventHandler(this.button4_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(37, 56);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(740, 299);
+            this.dataGridView1.Size = new System.Drawing.Size(825, 520);
             this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(486, 640);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Show Here";
+            // 
+            // BackBtn
+            // 
+            this.BackBtn.Location = new System.Drawing.Point(37, 12);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(110, 23);
+            this.BackBtn.TabIndex = 7;
+            this.BackBtn.Text = "Back";
+            this.BackBtn.UseVisualStyleBackColor = true;
+            this.BackBtn.Click += new System.EventHandler(this.BackBtn_Click);
+            // 
+            // L2
+            // 
+            this.L2.Location = new System.Drawing.Point(153, 12);
+            this.L2.Name = "L2";
+            this.L2.Size = new System.Drawing.Size(270, 23);
+            this.L2.TabIndex = 8;
+            this.L2.Text = "From Desc";
+            this.L2.UseVisualStyleBackColor = true;
+            // 
+            // L3
+            // 
+            this.L3.AutoSize = true;
+            this.L3.Location = new System.Drawing.Point(429, 17);
+            this.L3.Name = "L3";
+            this.L3.Size = new System.Drawing.Size(35, 13);
+            this.L3.TabIndex = 9;
+            this.L3.Text = "Stage";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(919, 665);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.BackBtn);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.UploadBtn);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -114,13 +148,15 @@ namespace ExOne
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button UploadBtn;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BackBtn;
+        private System.Windows.Forms.Button L2;
+        private System.Windows.Forms.Label L3;
     }
 }
 
--- ExOne/Forms/Form1.cs
+++ ExOne/Forms/Form1.cs
@@ -8,6 +8,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,43 +16,19 @@ namespace ExOne
 {
     public partial class Form1 : Form
     {
+        private Guid _id;
+        private int level;
+
         public Form1()
         {
             InitializeComponent();
         }
         public int ax = 0;
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-            var st = "AB" + ax.ToString();
-            ax++;
-
-            ProjList pl = new ProjList()
-            {
-                System = "My System",
-                ErrKind = st,
-                Desc = "Percobaan Lite DB",
-                Applicant = "Restu Adi ",
-                PIC = "Pradana",
-                ReqFormNo = "TSH-R2092100",
-                ApplyDate = "20/12/2020",
-                Stage = "initial",
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            };
-
-            //DbContext.GetInstance().GetCollection<ProjList>().Insert(pl);
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            var ads = DbContext.GetInstance().GetCollection<ProjList>().FindAll();
-            foreach (var itm in ads)
-            {
-                button2.Text = itm.ErrKind;
-            }
-            
+            dataGridView1.DataSource = GetAll(1);
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -86,86 +63,107 @@ namespace ExOne
         {
             var filePath = textBox1.Text;
             
-            if (filePath == null)
+            if (filePath == null || filePath == "")
             {
                 MessageBox.Show("Please select file first!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
-                var fileName = Path.GetFileName(filePath);
-                var excelFile = new FileInfo(filePath);
-                using (var p = new ExcelPackage(excelFile))
+                try
                 {
-                    var ws = p.Workbook.Worksheets.FirstOrDefault();
-                    var endrow = ws.Dimension.End.Row;
-                    var endcol = 14;
-
-                    for (int row = 2; row <= endrow; row++)
+                    var fileName = Path.GetFileName(filePath);
+                    var excelFile = new FileInfo(filePath);
+                    using (var p = new ExcelPackage(excelFile))
                     {
-                        List<string> kind = new List<string>();
-                        for (int col = 1; col <= endcol; col++)
+                        var ws = p.Workbook.Worksheets.FirstOrDefault();
+                        var endrow = ws.Dimension.End.Row;
+                        var endcol = 14;
+
+                        for (int row = 2; row <= endrow; row++)
                         {
-                            if (ws.Cells[row, col].Value != null)
+                            List<string> kind = new List<string>();
+                            for (int col = 1; col <= endcol; col++)
                             {
-                                kind.Add(ws.Cells[row, col].Value.ToString());
+                                if (ws.Cells[row, col].Value != null)
+                                {
+                                    kind.Add(ws.Cells[row, col].Value.ToString());
+                                }
+                                else
+                                {
+                                    kind.Add("");
+                                }
                             }
-                            else
+                            ProjList pl = new ProjList()
                             {
-                                kind.Add("");
+                                System = kind[0],
+                                ErrKind = kind[1],
+                                Desc = kind[2],
+                                Applicant = kind[3],
+                                PIC = kind[4],
+                                ReqFormNo = kind[5],
+                                ReqFormDesc = kind[6],
+                                Stage = kind[7],
+                                UserExpectedDate = kind[8],
+                                StageEstimateFinish = kind[9],
+                                StageActualFinish = kind[10],
+                                TestDateEstimate = kind[11],
+                                ApplyDate = kind[12],
+                                Memo = kind[13],
+                                FileName = fileName,
+                                CreatedAt = DateTime.Now,
+                                UpdatedAt = DateTime.Now
+                            };
+                            DbContext.GetInstance().GetCollection<ProjList>().Insert(pl);
+                            kind.Clear();
+                            Thread.Sleep(1); //avoid same time insert 
+                            if (ws.Cells[row + 1, 7].Value == null)
+                            {
+                                break;
                             }
-                        }
-                        ProjList pl = new ProjList()
-                        {
-                            System = kind[0],
-                            ErrKind = kind[1],
-                            Desc = kind[2],
-                            Applicant = kind[3],
-                            PIC = kind[4],
-                            ReqFormNo = kind[5],
-                            ReqFormDesc = kind[6],
-                            Stage = kind[7],
-                            UserExpectedDate = kind[8],
-                            StageEstimateFinish = kind[9],
-                            StageActualFinish = kind[10],
-                            TestDateEstimate = kind[11],
-                            ApplyDate = kind[12],
-                            Memo = kind[13],
-                            FileName = fileName,
-                            CreatedAt = DateTime.Now,
-                            UpdatedAt = DateTime.Now
-                        };
-                        DbContext.GetInstance().GetCollection<ProjList>().Insert(pl);
-                        kind.Clear();
-                        if (ws.Cells[row + 1, 7].Value == null)
-                        {
-                            break;
                         }
                     }
+                    MessageBox.Show("File " + fileName + " has been uploaded", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Detail: " + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
             }
         }
 
-        private List<ProjList> GetAll()
+        private List<ProjList> GetAll(int a) // L1
         {
             var list = new List<ProjList>();
             
             var col = DbContext.GetInstance().GetCollection<ProjList>();
-            //select * from K_tbl where k18 in (select max(k18) from K_tbl group by k06, k08)
-            var awx = (from x in col.FindAll().OrderByDescending(d => d.CreatedAt)
-                       group x by new { x.ReqFormNo, x.Stage }
-                      into xx
-                      select new { 
-                          //ReqFormNo = xx.Key.ReqFormNo, 
-                          //dt = (from x2 in xx select x2.CreatedAt).Max() 
-                          ReqNo = xx.Key,
-                          DT = xx.Max(m => m.CreatedAt)
-                      })
-                      .ToArray();
-            
-            var detx = from x in col.FindAll()
-                       where awx.Any(m => m.DT == x.CreatedAt)
+            //select * from K_tbl where CreatedAt in (select max(CreatedAt) from K_tbl group by ReqFormNo, ReqFormDesc, Stage)
+
+            List<DateTime> awx = new List<DateTime>();
+
+            if (a == 1)
+            {
+                awx = (from x in col.FindAll().OrderByDescending(d => d.CreatedAt)
+                        group x by new { x.ReqFormNo, x.ReqFormDesc}
+                        into xx
+                        select xx.Max(m => m.CreatedAt)
+                        ).ToList();
+            }
+            else if (a == 2)
+            {
+                awx = (from x in col.FindAll().OrderByDescending(d => d.CreatedAt)
+                        group x by new { x.ReqFormNo, x.ReqFormDesc, x.Stage }
+                        into xx
+                        select xx.Max(m => m.CreatedAt)
+                        ).ToList();
+            }
+
+            var detx = from x in col.FindAll().OrderByDescending(d => d.ReqFormNo)
+                       where awx.Contains(x.CreatedAt)
                        select x;
-            foreach (ProjList _id in col.FindAll())
+
+
+            foreach (ProjList _id in detx)
             {
                 list.Add(_id);
             }
@@ -173,9 +171,90 @@ namespace ExOne
             return list;
         }
 
+        private List<L1list> GetHeaderData()
+        {
+            var list = new List<L1list>();
+            var all = GetAll(1);
+
+            foreach ( var idx in all)
+            {
+                L1list l1 = new L1list()
+                {
+                    _id = idx.id,
+                    Applicant = idx.Applicant,
+                    ITPIC = idx.PIC,
+                    RequestFrom = idx.ReqFormNo,
+                    Description = idx.ReqFormDesc
+                };
+                list.Add(l1);
+            }
+
+            return list;
+        }
+
+        private void Get1List()
+        {
+            dataGridView1.DataSource = GetHeaderData();
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[1].Width = 75;
+            dataGridView1.Columns[2].Width = 75;
+            dataGridView1.Columns[3].Width = 120;
+            dataGridView1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            level = 1;
+        }
+
+        private void Get2List()
+        {
+            var list = new List<L1list>();
+            var all = GetAll(2);
+            var ab = (from a in all
+                     where a.id == _id
+                     select a).FirstOrDefault();
+            var ac = (from b in all
+                     where b.ReqFormDesc == ab.ReqFormDesc && b.ReqFormNo == ab.ReqFormNo
+                     select b).ToList();
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+            dataGridView1.DataSource = ac;
+            dataGridView1.Columns[0].Visible = false;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = GetAll();
+            Get1List();
         }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (level == 1)
+            {
+                label1.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                _id = Guid.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                Get2List();
+                level++;
+            }
+        }
+
+        private void BackBtn_Click(object sender, EventArgs e)
+        {
+            if (level == 2)
+            {
+                Get1List();
+                //level--;
+            }
+            else if (level == 1)
+            {
+                Form1_Load(this, null);
+            }
+        }
+    }
+
+    public class L1list
+    {
+        public Guid _id { get; set; }
+        public string Applicant { get; set; }
+        public string ITPIC { get; set; }
+        public string RequestFrom { get; set; }
+        public string Description { get; set; }
     }
 }
